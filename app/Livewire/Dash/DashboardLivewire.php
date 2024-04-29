3f0fe3af07c83b6b6a702213e5e9c97ef2d43469
EJyoungc <nerst@live.com>
2024-04-29 12:35:14 +0200

    main

--- app/Livewire/Dash/DashboardLivewire.php
+++ app/Livewire/Dash/DashboardLivewire.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Dash;
 
+use App\Models\Order;
+use App\Models\Product;
 use Livewire\Component;
 
 class DashboardLivewire extends Component
 {
     public function render()
     {
-        return view('livewire.dash.dashboard-livewire');
+        $orders  = Order::all();
+        $products = Product::all();
+        $Revenue =Order::sum('total');
+        return view('livewire.dash.dashboard-livewire')->with('orders',$orders)->with('products',$products)->with('Revenue',$Revenue);
     }
 }
